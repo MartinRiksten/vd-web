7dbed223d223d81634230b47ab086e2e7a87b28b
MartinRiksten <mriksten@rdw.nl>
2019-09-17 19:59:46 +0200

    Setup Package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,6 +3,15 @@ import "./attributes/clipboard";
 import "./attributes/draggable";
 import "./attributes/popover";
 import "./attributes/tooltip";
+import "./components/bootstrap-select";
+import "./components/vd-bool-icon";
+import "./components/vd-button";
+import "./components/vd-data";
+import "./components/vd-route";
+import "./components/vd-td";
+import "./components/vd-td-abbrev";
+import "./components/vd-td-list";
+import "./components/vd-th";
 export declare class VdWebInitializer {
     configure(aurelia: Aurelia): void;
 }
