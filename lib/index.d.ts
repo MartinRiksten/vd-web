128beff2a2a34cdab0820320957e0e5b03fcf67f
MartinRiksten <mriksten@rdw.nl>
2019-09-17 17:22:21 +0200

    Setup Package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,6 @@
-import { FrameworkConfiguration } from "aurelia-framework";
+import { Aurelia } from "aurelia-framework";
 import "./attributes/clipboard";
 import "./attributes/draggable";
 import "./attributes/popover";
 import "./attributes/tooltip";
-/**
- * VdWebInitialize
- * configuraion: FrameworkConfiguration
- */
-export declare class VdWebInitializer {
-    initialize(configuration: FrameworkConfiguration): void;
-}
+export declare function configure(aurelia: Aurelia): void;
