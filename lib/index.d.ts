17fa20df92836d9caef5c9accc367aa2419ce950
MartinRiksten <mriksten@rdw.nl>
2019-09-17 17:13:10 +0200

    Setup Package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,12 @@
+import { FrameworkConfiguration } from "aurelia-framework";
 import "./attributes/clipboard";
 import "./attributes/draggable";
 import "./attributes/popover";
 import "./attributes/tooltip";
+/**
+ * VdWebInitialize
+ * configuraion: FrameworkConfiguration
+ */
+export declare class VdWebInitializer {
+    initialize(configuration: FrameworkConfiguration): void;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
+import { FrameworkConfiguration } from "aurelia-framework";
 import "./attributes/clipboard";
 import "./attributes/draggable";
 import "./attributes/popover";
 import "./attributes/tooltip";
+
+/**
+ * VdWebInitialize
+ * configuraion: FrameworkConfiguration 
+ */
+export class VdWebInitializer {
+    public initialize(configuration: FrameworkConfiguration): void {
+        configuration.globalResources(["attributes/clipboard", "attributes/draggable", "attributes/popover", "tooltip"]);
+    }
+}
