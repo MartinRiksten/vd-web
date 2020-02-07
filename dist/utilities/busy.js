dd583be5d431dcbdd106f410e844eb44c60773b2
MartinRiksten <mriksten@rdw.nl>
2020-02-07 10:03:38 +0100

    Setup Package

---
+++ dist/utilities/busy.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Busy = /** @class */ (function () {
+    function Busy() {
+        this.isBusy = 0;
+    }
+    return Busy;
+}());
+exports.Busy = Busy;
