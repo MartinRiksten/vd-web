f2ab7db5689a02648909ec5b0e315511b152032a
MartinRiksten <mriksten@rdw.nl>
2020-02-20 12:34:38 +0100

    Setup Package

---
+++ src/components/vd-td-link.ts
@@ -0,0 +1,20 @@
+import { bindable } from 'aurelia-framework';
+import { VdTd } from './vd-td';
+
+export class VdTdLink extends VdTd<string> {
+    @bindable public showIcon = true;
+    @bindable public href: string = '#';
+    @bindable public icon = 'fas fa-chevron-circle-right';
+    @bindable public clickTarget: string | undefined;
+    @bindable public clickHandler!: (event: Event) => void;
+
+    public onClick(event: Event) {
+        if (!!this.clickTarget) {
+            $(this.clickTarget).click();
+        }
+
+        if (!!this.clickHandler) {
+            this.clickHandler(event);
+        }
+    }
+}
