dd419d5c78cca7c2187a57ad1f6808251d3835c1
Martin Riksten <RIKSTEN@rdw.dev.infosupport.net>
2021-02-10 09:19:24 +0100

    Setup Package

--- dist/components/vd-form-data.js
+++ dist/components/vd-form-data.js
@@ -18,7 +18,7 @@ var VdFormData = /** @class */ (function () {
     }
     VdFormData.defaultLabelClass = 'font-weight-bold text-sm-flex-left text-md-flex-right col-xs-12 col-md-5 mb-1';
     VdFormData.defaultGroupClass = 'col-xs-12 col-md-7';
-    VdFormData.defaultDataClass = 'ml-3 ml-lg-2';
+    VdFormData.defaultDataClass = 'ml-3 ml-lg-2 my-1';
     __decorate([
         aurelia_framework_1.bindable,
         __metadata("design:type", Object)
--- src/components/vd-form-data.ts
+++ src/components/vd-form-data.ts
@@ -3,7 +3,7 @@
 export class VdFormData {
   public static defaultLabelClass = 'font-weight-bold text-sm-flex-left text-md-flex-right col-xs-12 col-md-5 mb-1';
   public static defaultGroupClass = 'col-xs-12 col-md-7';
-  public static defaultDataClass = 'ml-3 ml-lg-2';
+  public static defaultDataClass = 'ml-3 ml-lg-2 my-1';
   
   @bindable public labelClass = VdFormData.defaultLabelClass;
   @bindable public groupClass = VdFormData.defaultGroupClass;
